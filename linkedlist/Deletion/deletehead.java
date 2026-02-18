1ff1d0b1b9a6be0a03eede7ebf8dee76af5c5b77
Ichcha <mehrishiichcha@gmail.com>
2026-02-18 20:18:54 +0530

    deletion of head and tail in linked list

---
+++ linkedlist/Deletion/deletehead.java
@@ -0,0 +1,60 @@
+package Deletion;
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class deletehead {
+    private static Node convertArrtoLL(int[] arr){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i=1; i<arr.length  ; i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+
+    public static Node deleteHead(Node head){
+        if(head==null){
+            return head;
+        }
+        else{
+            head = head.next;
+            return head;
+        }
+    }
+
+    public static int removeTail(Node head){
+        Node temp = head;
+        while(temp.next.next!= null){
+            temp = temp.next;
+        }
+        temp.next = null;
+        return temp.data;
+    }
+
+        public static void main(String[] args){
+        int[] arr = {2,3,4,56,7};
+        Node head = convertArrtoLL(arr);
+        Node temp = head;
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+        //System.out.println("Length of linked list is: " + lengthofLL(head));
+        //System.out.println(deleteHead(head).data);
+        System.out.println(removeTail(head));
+    }
+}
